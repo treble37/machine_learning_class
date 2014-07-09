1ae12ea5c2d660346685440c79445d94780910d4
Bruce Park <bruce@binarywebpark.com>
2014-07-09 08:03:28 -0700

    plotting

--- mlclass-ex1/ex1.m
+++ mlclass-ex1/ex1.m
@@ -45,6 +45,9 @@ m = length(y); % number of training examples
 % Plot Data
 % Note: You have to complete the code in plotData.m
 plotData(X, y);
+plot(X, y, 'rx', 'MarkerSize', 10);
+ylabel('Profit in $10,000s');
+xlabel('Population of City in 10,000s');
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
