8f7c8a74acff77bb942c1866a3e35969bbd4b93e
Bruce Park <brucepark0@gmail.com>
2014-09-06 18:37:57 -0700

    week 7 ex 6

--- mlclass-ex6/dataset3Params.m
+++ mlclass-ex6/dataset3Params.m
@@ -8,8 +8,11 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
+%C = 1;
+%sigma = 0.3;
+
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,10 +26,26 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+%C_list = [0.01 0.03 0.1 0.3 1 3 10 30];
+%sigma_list = [0.01 0.03 0.1 0.3 1 3 10 30];
+%error_min = flintmax("single");
+%optimal_C = C_list(1);
+%optimal_sigma = sigma_list(1);
+%for i=1:length(C_list)
+%  for j=1:length(sigma_list)
+%    model= svmTrain(X, y, C_list(i), @(x1, x2) gaussianKernel(x1, x2, sigma_list(j))); 
+%    predictions = svmPredict(model, Xval);
+%    error = mean(double(predictions ~= yval));
+%    if error<error_min
+%      error_min = error
+%      optimal_C = C_list(i)
+%      optimal_sigma = sigma_list(j)
+%    end
+%  end
+%end
 
-
-
-
+%C=optimal_C
+%sigma = optimal_sigma
 
 
 % =========================================================================
